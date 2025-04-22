637ecf3c0b71e72ec552be8d74bc30581d94f605
viktorashi <ioanvictorstan@gmail.com>
2025-04-22 21:28:35 +0300

    facut basisu asta un pic mai bun

--- plotting/van-der-Pol-oscillator.py
+++ plotting/van-der-Pol-oscillator.py
@@ -8,8 +8,6 @@ show = pyplot.show
 
 dt = 0.01
 
-#pt vna-der-pol
-
 #absout tot codul de aici tre sa-i dai copy-paste ca sa incerci sa faci toata nebunia din nou cu alte functii si sa schimni valorile in functia de update() gen daor atat pare foarte usor de modularising dar pt ca nu stiu cum functioneaza globalele in python nu pot sa moara masa
 
 def init_vars():
@@ -23,14 +21,6 @@ def observe():
     xres.append(x)
     yres.append(y)
 
-def update():
-    global x,y, xres, yres
-    dx_dt = y
-    dy_dt = -r * (x**2 -1) * y -x 
-
-    nextx = x + dx_dt * dt
-    nexty = y + dy_dt * dt
-    x,y = nextx, nexty
 
 def plot_phase_space():
     init_vars()
@@ -43,13 +33,23 @@ def plot_phase_space():
     axis([-3,3,-3,3])
     title('r = ' + str(r))
 
-def plot_everything():
-    global r ,rs
-    rs = [-1, -0.1, 0, .1, 1] 
-    for i in range(len(rs)):
-        subplot(1,len(rs), i+1)
-        r = rs[i]
-        plot_phase_space()
-    show()
+############### AICI SCHIMBI ######################
 
-plot_everything()
+#r-urile pe care sa le schimbi sa vezi bifurcatia la ele daca e sau ba
+rs = [-1, -0.1, 0, .1, 1] 
+
+def update():
+    global x,y, xres, yres
+    dx_dt = y
+    dy_dt = -r * (x**2 -1) * y -x 
+
+    nextx = x + dx_dt * dt
+    nexty = y + dy_dt * dt
+    x,y = nextx, nexty
+
+
+for i in range(len(rs)):
+    subplot(1,len(rs), i+1)
+    r = rs[i]
+    plot_phase_space()
+show()
